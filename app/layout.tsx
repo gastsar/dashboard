204188f3d9927c7b5c0b53487c675d4853d344c5
Kevine_dev <kevinarson_1996@yahoo.fr>
2024-05-29 22:33:05 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import { inter } from '@/app/ui/font';
+import '@/app/ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="fr">
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
